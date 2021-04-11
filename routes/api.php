92488cf83aa8b91b8eb416249c18c10cec8e43f1
sasha <aka.kisler@gmail.com>
2021-04-11 21:16:45 +0500

    fix 0.4

--- routes/api.php
+++ routes/api.php
@@ -28,9 +28,9 @@ Route::namespace('Api')->group(function () {
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-Route::apiResources(
-    [
-        'user' => 'API\UserController'
-    ]
-  );
+//Route::apiResources(
+//    [
+//        'user' => 'API\UserController'
+//    ]
+//  );
 });
